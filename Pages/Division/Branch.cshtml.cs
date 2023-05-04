10d4d1beb33a12396a537557c302507b7ef0809e
muzalee <85381407+muzalee@users.noreply.github.com>
2023-05-05 00:10:06 +0800

    display branch list onload

--- Pages/Division/Branch.cshtml.cs
+++ Pages/Division/Branch.cshtml.cs
@@ -1,11 +1,21 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Diagnostics;
+using System.Data.SqlClient;
+using spl.Model;
+using Microsoft.AspNetCore.Mvc;
 
 namespace spl.Pages.Division
 {
     public class BranchModel : PageModel
     {
+        private readonly IConfiguration _configuration;
         public string? Layout { get; private set; }
+        public List<Bahagian> listBahagian = new List<Bahagian>();
+
+        public BranchModel(IConfiguration config)
+        {
+            _configuration = config;
+        }
 
         public void OnGet()
         {
@@ -19,11 +29,39 @@ namespace spl.Pages.Division
             {
                 Layout = "../Shared/_UrusetiaLayout.cshtml";
             }
+
+            OnGetFetchBranch();
         }
 
-        public void OnGetFetchBranch()
+        public JsonResult OnGetFetchBranch()
         {
             Debug.WriteLine("Branch OnGetFetchBranch: Fetch branch list");
+            try
+            {
+                String connectionString = _configuration.GetConnectionString("DefaultConnection");
+                using SqlConnection connection = new(connectionString);
+                connection.Open();
+                String sql = "SELECT * FROM bahagian";
+
+                using SqlCommand command = new(sql, connection);
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Bahagian bahagian = new Bahagian();
+                        bahagian.Id = reader.GetInt32(0);
+                        bahagian.NamaBahagian = reader.GetString(1);
+                        listBahagian.Add(bahagian);
+                    }
+                }
+
+                connection.Close();
+                return new JsonResult(new { success = true });
+            }
+            catch (Exception ex)
+            {
+                return new JsonResult(new { success = false, msg = ex.Message });
+            }
 
         }
 
