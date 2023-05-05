5daa6ee3636a3630a26f30450a8a85bdf7bb4fda
muzalee <85381407+muzalee@users.noreply.github.com>
2023-05-05 21:06:09 +0800

    add course to officer

--- Model/Kursus.cs
+++ Model/Kursus.cs
@@ -3,10 +3,10 @@
     public class Kursus
     {
         public int? Id { get; set; }
-        public string Tajuk { get; set; }
+        public String Tajuk { get; set; }
         public String TarikhMula { get; set; }
         public String TarikhAkhir { get; set; }
-        public string Lokasi { get; set; }
+        public String Lokasi { get; set; }
         public bool? IsDeleted { get; set; }
         public int? IdKategori { get; set; }
         public KategoriKursus? KategoriKursus { get; set; }
